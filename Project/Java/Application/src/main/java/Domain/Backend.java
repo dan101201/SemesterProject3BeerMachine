5d0ecb305a4c44e75ae4599b28177590d7dba71c
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2022-10-31 13:26:04 +0100

    JavaBridge import

--- Project/Java/Application/src/main/java/Domain/Backend.java
+++ Project/Java/Application/src/main/java/Domain/Backend.java
@@ -1,4 +1,7 @@
 package Domain;
 
 public class Backend {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
--- Project/Java/Application/src/main/java/Domain/PHPConnection.java
+++ Project/Java/Application/src/main/java/Domain/PHPConnection.java
@@ -1,5 +1,21 @@
 package Domain;
 
-public class PHPConnection {
+import php.java.bridge.http.JavaBridgeRunner;
 
+public class PHPConnection {
+	public static final String JAVABRIDGE_PORT="8087";
+	static final JavaBridgeRunner runner = JavaBridgeRunner.getInstance(JAVABRIDGE_PORT);
+	
+	public static void main(String args[]) throws Exception {
+		try {
+			runner.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
+	
+	public void hello(String args[]) {
+		System.out.println("Hello World!");
+	}
 }
