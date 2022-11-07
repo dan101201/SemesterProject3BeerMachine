1c8b1d26ec838f06dbf0e1e43cd0d705361c3a5a
Marcus Skriver <marcus@familienskriver.dk>
2022-11-07 11:37:58 +0100

    Spark works!

--- Project/Java/Application/src/main/java/Domain/PHPConnection.java
+++ Project/Java/Application/src/main/java/Domain/PHPConnection.java
@@ -4,17 +4,31 @@ import php.java.bridge.http.JavaBridgeRunner;
 
 import static spark.Spark.*;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 public class PHPConnection {
 	public static final String JAVABRIDGE_PORT="8087";
 	static final JavaBridgeRunner runner = JavaBridgeRunner.getInstance(JAVABRIDGE_PORT);
 	
 	public static void main(String args[]) throws Exception {
-		try {
+		/*try {
 			runner.waitFor();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.exit(0);
+		System.exit(0);*/
+		
+		get("/hello", (req, res) -> {
+			return "Hello World";
+		});
+		
+		post("/hello", (req, res) -> {
+			return req.body();
+		});
+		
+		//init();
 	}
 	
 	public void hello(String args[]) {
