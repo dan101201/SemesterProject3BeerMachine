f244f20c202de27c7e2c877cacea436e6529858b
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2022-11-21 10:01:13 +0100

    batch finished

--- Project/Java/Application/src/main/java/Data/Batch.java
+++ Project/Java/Application/src/main/java/Data/Batch.java
@@ -1,5 +1,11 @@
 package Data;
 
-public record Batch() {
+import org.eclipse.milo.opcua.stack.core.types.builtin.DateTime;
+
+import java.util.LinkedList;
+
+public record Batch(
+		int recipeId, Machine machine, int totalBeers, int productionSpeed, int failedBeers, int successfulBeers,
+		LinkedList<SensorData> sensorData) {
 	
 }
