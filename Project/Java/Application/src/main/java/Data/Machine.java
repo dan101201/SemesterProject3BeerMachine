fe44056d4b6ff6bc05277fa17395cfda75fd23e9
Nikolaj <nthom21@student.sdu.dk>
2022-11-21 11:18:10 +0100

    Auto stash before merge of "main" and "origin/main"

--- Project/Java/Application/src/main/java/Data/Machine.java
+++ Project/Java/Application/src/main/java/Data/Machine.java
@@ -2,13 +2,8 @@ package Data;
 
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
-import org.eclipse.milo.opcua.sdk.client.api.identity.IdentityProvider;
-import org.eclipse.milo.opcua.sdk.client.api.identity.UsernameProvider;
 import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
-import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
-import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
-import org.eclipse.milo.opcua.stack.core.types.enumerated.TimestampsToReturn;
 import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
 import org.eclipse.milo.opcua.stack.core.util.EndpointUtil;
 
@@ -17,43 +12,45 @@ import java.util.List;
 
 public class Machine {
     URI machineAddress;
-    String name = "";
+    String name;
+    OpcUaClient client;
 
-    public Machine (URI machineAddress, String name) {
-        this.machineAddress=machineAddress;
-        this.name=name;
+    public Machine(URI machineAddress, String name) {
+        this.machineAddress = machineAddress;
+        this.name = name;
     }
-    public Machine () {
-    }
-    public static void main(String[] args) {
+
+    public Machine() {
         String hostname = "127.0.0.1";
         int port = 4840;
         String endpointUrl = "opc.tcp://127.0.0.1:4840";
 
-        try
-
-        {
+        try {
             List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints(endpointUrl).get();
 
             OpcUaClientConfigBuilder cfg = new OpcUaClientConfigBuilder()
 //                    .setIdentityProvider(getIdentityprovider())
-;
+                    ;
             EndpointDescription configPoint = EndpointUtil.updateUrl(endpoints.get(0), hostname, port);
             cfg.setEndpoint(configPoint);
-            OpcUaClient client = OpcUaClient.create(cfg.build());
+            client = OpcUaClient.create(cfg.build());
             client.connect().get();
-            System.out.println("forbundet! "+ client.getNamespaceTable());
-        }
-        catch(Throwable ex)
-        {
-            System.out.println("*****************************************");
+            System.out.println("forbundet! " + client.getNamespaceTable());
+        } catch (Throwable ex) {
+            System.out.println("COULD NOT CONNECT");
             ex.printStackTrace();
         }
 
     }
 
+    public static void main(String[] args) {
+
+    }
+
     public float getTemperature() {
-        return 0;
+        NodeId nodeId = new NodeId(6, "::Program:Data.Value.Temperature");
+
+        return;
     }
 
     public String getName() {
