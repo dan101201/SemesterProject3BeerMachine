f81beff037466124052f58eca8c03ee3f844c703
Tioman99 <hluc626@gmail.com>
2022-11-21 10:08:58 +0100

    record

--- Project/Java/Application/src/main/java/Data/Record.java
+++ Project/Java/Application/src/main/java/Data/Record.java
@@ -1,5 +1,7 @@
 package Data;
 
+import Domain.Notifications;
+
 import java.util.LinkedList;
 import java.util.UUID;
 
@@ -7,14 +9,16 @@ public class Record {
 
     private UUID recordId;
     private Machine machine;
-    public LinkedList<Batch> data;
-    
+    public LinkedList<Batch> dataBatch;
+    public LinkedList<SensorData> sensorData;
+    public LinkedList<Notifications> notifications;
 
-    /*- recordID: UUID
-- machine: Machine
-+ data: LinkedList<Batch>
-+ sensor: LinkedList<SensorData>
-+ notifications: linkedList<Notification>
-*/
+    public Record (UUID recordId, Machine machine, LinkedList<Batch> dataBatch, LinkedList<SensorData> sensorData, LinkedList<Notifications> notifications) {
+        this.recordId = recordId;
+        this.machine = machine;
+        this.dataBatch = dataBatch;
+        this.sensorData = sensorData;
+        this.notifications = notifications;
+    };
 
 }
