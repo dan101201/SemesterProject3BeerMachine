3a1aedf4e3cb316e46d947b654b71b7029ede33d
baremikkel <75213352+baremikkel@users.noreply.github.com>
2022-11-21 13:39:11 +0100

    LMAO EZ L + RATIO + S + Q + R +T

--- Project/Frontend/dist/js/sensorRead.js
+++ Project/Frontend/dist/js/sensorRead.js
@@ -1,15 +1,10 @@
-async function func(){
-/*    const Http = new XMLHttpRequest();
-const url = 'http://localhost:4567/machine/0/temperature';
-Http.open("GET", url);
-Http.send();
-Http.onreadystatechange=(e)=>{
-    console.log(Http.responseText);
-}*/
+async function tempFunc(){
 
     const response = await fetch('http://localhost:4567/machine/0/temperature').then((res) => res.json()).catch((err) => {
         console.error(err);
     })
-    console.log(response)
+    addDataT(tempChart, response)
 }
+
+setInterval(async() => tempFunc(), 1000)
 
