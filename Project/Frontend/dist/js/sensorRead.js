3b748b8e1d5c839f211652a99978a787753e6a55
baremikkel <75213352+baremikkel@users.noreply.github.com>
2022-11-21 13:30:42 +0100

    :()

--- Project/Frontend/dist/js/sensorRead.js
+++ Project/Frontend/dist/js/sensorRead.js
@@ -1,10 +1,15 @@
-function func(){
-    const Http = new XMLHttpRequest();
+async function func(){
+/*    const Http = new XMLHttpRequest();
 const url = 'http://localhost:4567/machine/0/temperature';
 Http.open("GET", url);
 Http.send();
 Http.onreadystatechange=(e)=>{
     console.log(Http.responseText);
-}
+}*/
+
+    const response = await fetch('http://localhost:4567/machine/0/temperature').then((res) => res.json()).catch((err) => {
+        console.error(err);
+    })
+    console.log(response)
 }
 
