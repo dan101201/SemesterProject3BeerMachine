549e7593b16e6021139d1638f389707bdd78f15b
baremikkel <mikkelo.privat@gmail.com>
2022-11-28 12:41:47 +0100

    Added async functions

--- Project/Frontend/dist/js/sensorRead.js
+++ Project/Frontend/dist/js/sensorRead.js
@@ -6,5 +6,20 @@ async function tempFunc(){
     addDataT(tempChart, response)
 }
 
+async function humidFunc(){
+    const response = await fetch('http://localhost:4567/machine/0/humidity').then((res) => res.json()).catch((err) => {
+        console.error(err);
+})
+    addDataH(humidFunc, response)
+}
+
+async function vibraFunc(){
+    const response = await fetch('http://localhost:4567/machine/0/vibration').then((res) => res.json()).catch((err) => {
+        console.error(err);
+})
+    addDataV(vibraFunc, response)
+}
 setInterval(async() => tempFunc(), 1000)
+setInterval(async() => humidFunc(), 1000)
+setInterval(async() => vibraFunc(), 1000)
 
