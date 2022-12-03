19be0c003e22986a3a7773477f286cbbfe5f95bc
Marcus Skriver <marcus@familienskriver.dk>
2022-12-03 18:14:58 +0100

    minor changes

--- Project/Frontend/startServer.bat
+++ Project/Frontend/startServer.bat
@@ -3,6 +3,6 @@ set /A port=8080
 
 docker build --pull --rm -f "Dockerfile" -t beerfrontend:latest "."
 
-docker run -d -p 8080:80 --rm --name HttpServer frontend:latest
+docker run -d -p %port%:80 --rm --name HttpServer frontend:latest
 
 START http://localhost:%port%
