d284c4b6a196d8c04192c2da82eec69b9c127245
Kochemanden <olkoc21@student.sdu.dk>
2022-12-10 18:25:17 +0100

    Ændret Alert filen
    Nu kan alarmerne også bruges til at stoppe produktion istedet for kun at advare om fejl.

--- Project/Frontend/dist/js/alert.js
+++ Project/Frontend/dist/js/alert.js
@@ -1,23 +1,38 @@
 let button = document.getElementById("StartButton");
 
 button.addEventListener('click', () => {
-    checkAmount();
+    let arrayBooleans = [];
+    arrayBooleans.push(alertNotification("barley"));
+    arrayBooleans.push(alertNotification("hops"));
+    arrayBooleans.push(alertNotification("malt"));
+    arrayBooleans.push(alertNotification("wheat"));
+    arrayBooleans.push(alertNotification("yeast"));
+    arrayBooleans.push(maintainanceAlert());
+    
+    if(arrayBooleans.includes(false)){
+        console.log("Jeg fandt en fejl"); // Hvis den finder mindst en fejl, så lader den være med at køre. (Her kører vi så "printer logik" altså¨det kan godt var den givne opskrift ikke bruger en bestemt ingredient som er ved at løbe tør, men programmet vil stadig ikke starte uden)
+    } else{
+        console.log("Ingen fejl her makker :)"); // Hvis ingen fejl, så er det her den så sender request'en om at lave et batch videre til maskinen.
+    }
 })
 
-function checkAmount(){
 
-    alertNotification("barley");
-    alertNotification("hops");
-    alertNotification("malt");
-    alertNotification("wheat");
-    alertNotification("yeast");
-}
-
-
+// Har gjort sådan at den returnere en boolean, så man også kan bruge den til at forhindre kode.
 function alertNotification(ingredient){
     var amount = document.getElementById(ingredient).value;
     let minimum = 50;
     if (amount < minimum){
         alert("Amount of " + ingredient + " is getting low! Please fix");
+        return false;
     }
-};
+    return true;
+}
+
+function maintainanceAlert(){
+    var timer = document.getElementById("maintain").value;
+    if (timer < 20){
+        alert("Machine is in need of fixing");
+        return false;
+    }
+    return true;
+}
