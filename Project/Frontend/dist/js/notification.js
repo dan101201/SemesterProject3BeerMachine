237e5c5822c03db79156e242c0a6b3283d54ddf4
baremikkel <75213352+baremikkel@users.noreply.github.com>
2022-12-14 16:36:21 +0100

    notification and HTTPConnection ready for test

--- Project/Frontend/dist/js/notification.js
+++ Project/Frontend/dist/js/notification.js
@@ -1,16 +1,54 @@
-function addToWarningTable(warning, timestamp, mNumber){
-var table = document.getElementById("warningTable");
-id = table.rows.length-1;
-var row = table.insertRow(1);
+function addToWarningTable(warning, timestamp, mNumber) {
+    var table = document.getElementById("warningTable");
+    id = table.rows.length - 1;
+    var row = table.insertRow(1);
 
-var idCell = row.insertCell(0);
-var warningCell = row.insertCell(1);
-var timestampCell = row.insertCell(2);
-var mNumberCell = row.insertCell(3);
+    var idCell = row.insertCell(0);
+    var warningCell = row.insertCell(1);
+    var timestampCell = row.insertCell(2);
+    var mNumberCell = row.insertCell(3);
 
-idCell.innerHTML = id;
-warningCell.innerHTML = warning;
-timestampCell.innerHTML = timestamp;
-mNumberCell.innerHTML = mNumber;
+    idCell.innerHTML = id;
+    warningCell.innerHTML = warning;
+    timestampCell.innerHTML = timestamp;
+    mNumberCell.innerHTML = mNumber;
+
+}
+var row
+var idCell;
+var recipeCell;
+var speedCell
+var processedCell;
+var acceptedCell;
+var declinedCell;
+var machineCell;
+var timestampCell;
+var currentId = 0;
+
+function updateCurrentBatch(processed, accepted, declined, timestamp) {
+    var table = document.getElementById("currentTable")
+    currentId++
+
+    if (table.rows.length < 2) {
+        row = table.insertRow(1);
+        console.log(table.rows.length)
+        idCell = row.insertCell(0);
+        recipeCell = row.insertCell(1);
+        speedCell = row.insertCell(2)
+        processedCell = row.insertCell(3);
+        acceptedCell = row.insertCell(4);
+        declinedCell = row.insertCell(5);
+        machineCell = row.insertCell(6);
+        timestampCell = row.insertCell(7);
+    }    
+
+    idCell.innerHTML = currentId;
+    recipeCell.innerHTML = "RecipeCell"
+    speedCell.innerHTML = "SpeedCell";
+    processedCell.innerHTML = processed
+    acceptedCell.innerHTML = accepted
+    declinedCell.innerHTML = declined
+    machineCell.innerHTML = "0"
+    timestampCell.innerHTML = timestamp
 
 }
