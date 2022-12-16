fca29ad145bee282b009c314f041cc9c1c410f35
Tioman99 <hluc626@gmail.com>
2022-12-16 15:00:30 +0100

    commented oee

--- Project/Java/Application/src/main/java/Domain/Backend.java
+++ Project/Java/Application/src/main/java/Domain/Backend.java
@@ -21,6 +21,15 @@ public class Backend {
 		return strings.toArray(new String[0]);
 	}
 
+	/* public float getOEE(int machineId) { //guess we aint doing this
+		private float myOEE;
+		private float goodBeers = new float (machines.get(machineId).getGood()); //converting getGood to float
+		private float cycleTime = new float (machines.get(machineId).getCycleTime()); //these methods shall be made
+		private float plannedTime = new float (machines.get(machineId).getPannedTime());
+		myOEE = goodBeers * cycleTime / plannedTime
+				return myOEE;
+	} */
+
 	public void setMachineSpeed(int i, float j) {
 		machines.get(0).setMachineSpeed(i, j);
 	}
