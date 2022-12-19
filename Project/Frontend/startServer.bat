8702ae16361fc56485c23fc2aecda89a7308aa24
Marcus Skriver <marcus@familienskriver.dk>
2022-12-19 11:40:54 +0100

    Docker networking works

--- Project/Frontend/startServer.bat
+++ Project/Frontend/startServer.bat
@@ -1,5 +1,5 @@
 docker build --pull --rm -f "Dockerfile" -t beerfrontend:latest "."
 
-docker run -d -p 8080:80 --rm --net beer-network --name HttpServer -it frontend:latest
+docker run -d -p 8080:80 --ip 172.18.0.123 --net beer-network --name BeerFrontend -it beerfrontend:latest
 
 START http://localhost:8080
