6ebcc4871aa08f497b4357c09b679c53a6f77783
Tioman99 <90039319+Tioman99@users.noreply.github.com>
2022-12-21 10:48:57 +0100

    getOee

--- Project/Frontend/dist/js/notification.js
+++ Project/Frontend/dist/js/notification.js
@@ -14,7 +14,7 @@ function addToWarningTable(warning, timestamp, mNumber) {
     mNumberCell.innerHTML = mNumber;
 
 }
-var row
+var row;
 var idCell;
 var recipeCell;
 var speedCell
@@ -24,7 +24,12 @@ var declinedCell;
 var machineCell;
 var timestampCell;
 var currentId = 0;
-var startTimestamp
+var startTimestamp;
+var oeeCell;
+
+function getOee(accepted, idealCycle, plannedTime) {
+    return accepted*idealCycle/plannedTime;
+}
 
 function resetCurrentBatchTable(){
     document.getElementById("currentTable").deleteRow(1);
@@ -33,8 +38,8 @@ function resetCurrentBatchTable(){
 }
 
 function updateCurrentBatchTable(processed, accepted, declined) {
-    var table = document.getElementById("currentTable")
-    currentId++
+    var table = document.getElementById("currentTable");
+    currentId++;
     if(getSpeed() != 0) {
         if (table.rows.length < 2) {
             row = table.insertRow(1);
@@ -50,13 +55,30 @@ function updateCurrentBatchTable(processed, accepted, declined) {
             startTimestamp.innerHTML = getDate();
             recipeCell.innerHTML = getRecipe();
             speedCell.innerHTML = getSpeed();
-        }    
+        }
     
         idCell.innerHTML = currentId;
-        processedCell.innerHTML = processed
-        acceptedCell.innerHTML = accepted
-        declinedCell.innerHTML = declined
-        machineCell.innerHTML = "0"
-        timestampCell.innerHTML = getDate()
+        processedCell.innerHTML = processed;
+        acceptedCell.innerHTML = accepted;
+        declinedCell.innerHTML = declined;
+        machineCell.innerHTML = "0";
+        timestampCell.innerHTML = getDate();
     }
+}
+
+function updateLastBatchTable() {
+    var table = document.getElementById("oldTable");
+        row = table.insertRow(currentId+1);
+        idCell = row.insertCell(0);
+        recipeCell = row.insertCell(1);
+        speedCell = row.insertCell(2)
+        processedCell = row.insertCell(3);
+        acceptedCell = row.insertCell(4);
+        declinedCell = row.insertCell(5);
+        machineCell = row.insertCell(6);
+        timestampCell = row.insertCell(7);
+        startTimestamp = row.insertCell(8);
+        oeeCell = row.insertCell(8);
+        oeeCell.innerHTML = getOee(acceptedCell, idealCycle, plannedTime);
+
 }
